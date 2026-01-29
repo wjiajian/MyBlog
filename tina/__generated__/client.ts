ae6a8e83191ff022d8f512572986c2fab809c86c
wjiajian <71640797+wjiajian@users.noreply.github.com>
2026-01-29 10:48:12 +0800

    feat: 移除旧的多余文件

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/Admin/Desktop/code/MyBlog/tina/__generated__/.cache/1769650968090', url: 'https://content.tinajs.io/2.1/content/4ed633c3-6c22-4d50-a0ac-01144dd358ce/github/main', token: 'f6aab4af7534a0dedc2f233aeaa6ded71d58cc12', queries,  });
-export default client;
-  
